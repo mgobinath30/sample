c4afcd4cf4b602e46d994f07d8950afc0afbbdd1
Suraya Shivji <surayashivji@gmail.com>
2017-10-06 15:36:50 -0400

    updates

--- js/app.js
+++ js/app.js
@@ -4,59 +4,48 @@ app.config(function($routeProvider, $locationProvider){
   $routeProvider
   .when('/potterpics',{
     templateUrl: "../pages/potter.html"
-    //animation: 'first animated zoomIn'
   })
   .when('/events',{
     templateUrl: "../pages/events.html"
-    //animation: 'first'
   })
   .when('/blaster',{
     templateUrl: "../pages/blaster.html"
-    //animation: 'first'
   })
   .when('/bayberry',{
     templateUrl: "../pages/bayberry.html"
-    //animation: 'first'
   })
   .when('/barberry',{
     templateUrl: "../pages/barberry.html"
-    //animation: 'first'
   })
   .when('/mares',{
     templateUrl: "../pages/mars.html"
-    //animation: 'first'
   })
   .when('/loxe',{
     templateUrl: "../pages/loxe.html"
-    //animation: 'first'
   })
   .when('/rizikitoto',{
     templateUrl: "../pages/rizikitoto.html"
-    //animation: 'first'
   })
   .when('/bilberry',{
     templateUrl: "../pages/bilberry.html"
-    //animation: 'first'
   })
   .when('/vaporposter',{
     templateUrl: "../pages/vaporposter.html"
-    //animation: 'first'
   })
   .when('/interaction',{
     templateUrl: "../pages/arweather.html"
-    //animation: 'first'
+  })
+  .when('/photogram',{
+    templateUrl: "../pages/photogram.html"
   })
   .when('/weeklymotion',{
     templateUrl: "../pages/weeklymotion.html"
-    //animation: 'first'
   })
   .when('/stitch',{
     templateUrl: "../pages/stitch.html"
-    //animation: 'first'
   })
   .when('/',{
    templateUrl: "../pages/grid.html"
-    //animation: 'second'
   })
   .otherwise({redirectTo: '/'});
 
@@ -64,7 +53,6 @@ app.config(function($routeProvider, $locationProvider){
   return {
     enter: function(element, done) {
       element.css('display', 'none');
-      //$('body').removeClass('gridcolorchange');
       element.fadeIn(500, done);
       return function() {
         element.stop();
@@ -72,14 +60,12 @@ app.config(function($routeProvider, $locationProvider){
     },
     leave: function(element, done) {
       element.fadeOut(500, done);
-      //$('body').addClass('gridcolorchange');
       return function() {
         element.stop();
       }
     }
   }
 });
-
 
 app.controller('ctrl', function($scope, $rootScope){
   $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute){
